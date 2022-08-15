3cb6e72b10925b5bf138df11e1d53c215c4a907b
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-15 15:28:44 +0200

    iteration 10 kinda works but doesnt

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -40,10 +40,33 @@ router.get('/movies/:id', (req, res) => {
 
 // POST movies/:id/delete
 router.post('/movies/:id/delete', (req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
     Movie.findByIdAndDelete(id)
         .then(() => res.redirect('/movies'))
         .catch((err) => console.log(err));
+});
+
+// GET movies/:id/edit
+router.get('/movies/:id/edit', (req, res) => {
+    const { id } = req.params;
+    Movie.findById(id)
+        .then((movie) => {
+            Celebrity.find()
+                .then((celebrities) => {
+                    res.render('movies/edit-movie', { movie, celebrities });
+                })
+                .catch((err) => console.log(err));
+        })
+        .catch((err) => console.log(err));
+});
+
+// POST movies/:id/edit
+router.post('/movies/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const { title, genre, plot, cast } = req.body;
+    Movie.findOneAndUpdate(id, { title, genre, plot, cast })
+        .then(() => res.redirect('/movies'))
+        .catch((err) => console.log(err)); 
 });
 
 module.exports = router; 
