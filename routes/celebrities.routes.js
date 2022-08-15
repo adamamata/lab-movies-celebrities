1732d6b250cde0a7e7606093967daa46ae232c8f
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-15 13:04:00 +0200

    iteration 4 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,18 +1,29 @@
 const router = require('express').Router();
 const Celebrity = require('../models/Celebrity.model');
 
+// GET celebrities/create
 router.get('/celebrities/create', (req, res) => {
     res.render('celebrities/new-celebrity');
 });
 
+// POST celebrities/create
 router.post('/celebrities/create', (req, res) => {
     const { name, occupation, catchPhrase } = req.body;
     Celebrity.create({ name, occupation, catchPhrase})
         .then(() => res.redirect('/celebrities'))
         .catch((err) => {
             console.log(err);
-            res.render('celebrities/new-celebrity');
+            res.redirect('celebrities/new-celebrity');
         })
+});
+
+// GET celebrities
+router.get('/celebrities', (req, res) => {
+    Celebrity.find()
+        .then((allCelebs) => {
+            res.render('celebrities/celebrities', { allCelebs });
+        })
+        .catch((err) => console.log(err));
 });
 
 module.exports = router; 
