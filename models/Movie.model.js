78b99cf62ca16ee4b7ce57d19094ca9189c1bb66
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-15 13:07:46 +0200

    iteration 5 done

---
+++ models/Movie.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;  
+
+const movieSchema = new Schema(
+    {
+        title: {type: String},
+        genre: {type: String},
+        plot: {type: String},
+        cast: [{type: Schema.Types.ObjectId, ref: "Celebrity"}]
+    }
+);
+
+module.exports = model('Movie', movieSchema);
