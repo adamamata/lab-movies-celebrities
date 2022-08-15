a9471afcd857d821cabbc9cdddf7ca651e51429f
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-15 12:43:06 +0200

    iteration 2 done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,12 @@
-//  Add your code here
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;  
+
+const celebritySchema = new Schema(
+    {
+        name: {type: String},
+        occupation: {type: String, defualt: 'Unkown'},
+        catchPhrase: {type: String}
+    }
+);
+
+module.exports = model('Celebrity', celebritySchema);
